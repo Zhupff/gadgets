d0a304f0defb0be43fb3a6f30f1543d5ee458add
Zhupf <zhupff@qq.com>
2024-08-19 16:58:14 +0800

    Add widget-dsl and compile.

--- gadget-basic/basic-compile/src/main/kotlin/zhupff/gadgets/basic/compile/KSP.kt
+++ gadget-basic/basic-compile/src/main/kotlin/zhupff/gadgets/basic/compile/KSP.kt
@@ -3,6 +3,7 @@ package zhupff.gadgets.basic.compile
 import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.processing.SymbolProcessorProvider
+import com.google.devtools.ksp.symbol.KSDeclaration
 
 abstract class KSP : SymbolProcessorProvider, SymbolProcessor {
 
@@ -11,4 +12,18 @@ abstract class KSP : SymbolProcessorProvider, SymbolProcessor {
     override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor = apply {
         this.processingEnv = environment
     }
+
+    protected val KSDeclaration.declarePackageName: String; get() = "${packageName.getQualifier()}.${packageName.getShortName()}"
+
+    protected val KSDeclaration.declareClassName: String; get() {
+        var name = simpleName.getShortName()
+        var declaration = parentDeclaration
+        while (declaration != null) {
+            name = "${declaration.simpleName.getShortName()}.${name}"
+            declaration = declaration.parentDeclaration
+        }
+        return name
+    }
+
+    protected val KSDeclaration.declareQualifiedName: String; get() = "${declarePackageName}.${declareClassName}"
 }
--- gadget-widget/widget-compile/src/main/kotlin/zhupff/gadgets/widget/compile/ksp/WidgetDslKsp.kt
+++ gadget-widget/widget-compile/src/main/kotlin/zhupff/gadgets/widget/compile/ksp/WidgetDslKsp.kt
@@ -2,11 +2,48 @@ package zhupff.gadgets.widget.compile.ksp
 
 import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.symbol.KSAnnotated
+import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.google.devtools.ksp.symbol.KSDeclaration
+import com.google.devtools.ksp.symbol.KSPropertyDeclaration
+import com.squareup.kotlinpoet.ksp.writeTo
 import zhupff.gadgets.basic.compile.KSP
+import zhupff.gadgets.widget.WidgetDsl
+import zhupff.gadgets.widget.compile.processor.WidgetDslProcessor
 
 class WidgetDslKsp : KSP() {
 
+    private val symbol2items = ArrayList<Pair<KSDeclaration, WidgetDslProcessor.Item>>()
+
     override fun process(resolver: Resolver): List<KSAnnotated> {
+        resolver.getSymbolsWithAnnotation(WidgetDsl::class.java.canonicalName).forEach { symbol ->
+            if (symbol is KSDeclaration) {
+                val alias = symbol.annotations.find {
+                    it.annotationType.resolve().declaration.declareQualifiedName == WidgetDsl::class.java.canonicalName
+                }?.arguments?.let { arguments ->
+                    arguments.find {
+                        it.name?.getShortName() == "alias"
+                    }?.value as? String
+                }
+                if (alias.isNullOrEmpty()) return@forEach
+                val canonicalName = when (symbol) {
+                    is KSClassDeclaration -> symbol.declareQualifiedName
+                    is KSPropertyDeclaration -> {
+                        symbol.type.resolve().declaration.declareQualifiedName
+                    }
+                    else -> null
+                }
+                if (canonicalName.isNullOrEmpty()) return@forEach
+                val packageName = symbol.declarePackageName
+                val item = WidgetDslProcessor.Item(alias, packageName, canonicalName)
+                symbol2items.add(symbol to item)
+            }
+        }
         return emptyList()
+    }
+
+    override fun finish() {
+        symbol2items.forEach { (symbol, item) ->
+            WidgetDslProcessor.buildFileSpec(item).writeTo(processingEnv.codeGenerator, true, listOf(symbol.containingFile!!))
+        }
     }
 }
