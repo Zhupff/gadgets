fd6b3a0aee8ceadf593da4e2311276a77bebe079
Zhupf <zhupff@qq.com>
2024-11-21 14:52:52 +0800

    优化 theme 模块

--- gadget-theme/theme/src/main/kotlin/zhupff/gadgets/theme/ThemeConfig.kt
+++ gadget-theme/theme/src/main/kotlin/zhupff/gadgets/theme/ThemeConfig.kt
@@ -9,4 +9,11 @@ open class ThemeConfig(
 
     open fun obtainAttribute(attributeName: String, resourceId: Int, resourceName: String, resourceType: String): ThemeAttribute? =
         attributesMap[attributeName]?.copy(resourceId, resourceName, resourceType)
+
+    open fun obtainAttribute(attributeName: String, resourceId: Int): ThemeAttribute? = obtainAttribute(
+        attributeName,
+        resourceId,
+        APPLICATION.resources.getResourceEntryName(resourceId),
+        APPLICATION.resources.getResourceTypeName(resourceId)
+    )
 }
