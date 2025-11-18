934f2bc37b6a58b12f238a9c74c10877565e8a1e
Zhupf <zhupff@qq.com>
2025-11-18 15:29:13 +0800

    framework(#0): 1)添加:gadget-widget:app示例

--- gadget-widget/app/src/main/kotlin/gadget/widget/app/MainActivity.kt
+++ gadget-widget/app/src/main/kotlin/gadget/widget/app/MainActivity.kt
@@ -1,6 +1,19 @@
 package gadget.widget.app
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import gadget.widget.app.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewBinding: MainActivityBinding by lazy {
+        MainActivityBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(viewBinding.root)
+        viewBinding.logo.setOnClickListener {
+        }
+    }
 }
